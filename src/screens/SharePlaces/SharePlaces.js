b8c97b47674192d6b8f653e23e664e283ef6eb7f
ralph1786 <ralph1786@gmail.com>
2019-05-15 15:57:07 -0400

    components that represent new screens

---
+++ src/screens/SharePlaces/SharePlaces.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export class SharePlacesScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Share Place Screen </Text>
+      </View>
+    );
+  }
+}
+
+export default SharePlacesScreen;
