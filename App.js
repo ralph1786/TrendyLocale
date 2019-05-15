d639f984c7af8c3f4e120a79f13c81fe6c7e8e54
ralph1786 <ralph1786@gmail.com>
2019-05-15 17:49:39 -0400

    registered new screen and connected them to redux

--- App.js
+++ App.js
@@ -2,18 +2,40 @@ import { Navigation } from "react-native-navigation";
 import AuthScreen from "./src/screens/Auth/Auth";
 import SharePlacesScreen from "./src/screens/SharePlaces/SharePlaces";
 import FindPlacesScreen from "./src/screens/FindPlaces/FindPlaces";
+import LocationDetailsScreen from "./src/screens/LocationDetails/LocationDetails";
+import { Provider } from "react-redux";
+import storeConfig from "./store/StoreConfig";
 
-//Register Screens will only show
+const store = storeConfig();
+
+//Only Registered Screens Will Show
 
 //first argument is an unique id for the screen
-Navigation.registerComponent("trendy-locale.AuthScreen", () => AuthScreen);
+//second argument is a function that returns the component
+//third argument is the redux store
+//fourth argument is the Provider.
+Navigation.registerComponent(
+  "trendy-locale.AuthScreen",
+  () => AuthScreen,
+  store,
+  Provider
+);
 Navigation.registerComponent(
   "trendy-locale.SharePlacesScreen",
-  () => SharePlacesScreen
+  () => SharePlacesScreen,
+  store,
+  Provider
 );
 Navigation.registerComponent(
   "trendy-locale.FindPlacesScreen",
-  () => FindPlacesScreen
+  () => FindPlacesScreen,
+  store,
+  Provider
+);
+
+Navigation.registerComponent(
+  "trendy-locale.LocationDetailsScreen",
+  () => LocationDetailsScreen
 );
 
 //Start a App
