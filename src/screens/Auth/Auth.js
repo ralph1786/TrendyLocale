5a8f6ac0a3cd6025b928968d5268155e2389896f
ralph1786 <ralph1786@gmail.com>
2019-05-15 15:56:27 -0400

    button will display other screens

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+
+import startTab from "../MainTabs/startMainTabs";
 
 class AuthScreen extends Component {
+  loginHandler = () => {
+    startTab();
+  };
+
   render() {
     return (
       <View>
         <Text>Auth Screen</Text>
+        <Button title="Login" onPress={this.loginHandler} />
       </View>
     );
   }
