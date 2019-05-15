32f0c1783d3978d911f4aeb996abd044f04c654d
ralph1786 <ralph1786@gmail.com>
2019-05-15 15:55:42 -0400

    registered new screens

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { Navigation } from "react-native-navigation";
 import AuthScreen from "./src/screens/Auth/Auth";
+import SharePlacesScreen from "./src/screens/SharePlaces/SharePlaces";
+import FindPlacesScreen from "./src/screens/FindPlaces/FindPlaces";
 
 //Register Screens will only show
 
 //first argument is an unique id for the screen
 Navigation.registerComponent("trendy-locale.AuthScreen", () => AuthScreen);
+Navigation.registerComponent(
+  "trendy-locale.SharePlacesScreen",
+  () => SharePlacesScreen
+);
+Navigation.registerComponent(
+  "trendy-locale.FindPlacesScreen",
+  () => FindPlacesScreen
+);
 
 //Start a App
 
