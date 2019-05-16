315b6069a1e2453a32b71e2d17739c762ac2a2a8
ralph1786 <ralph1786@gmail.com>
2019-05-16 11:30:56 -0400

    newly created side menu

---
+++ src/screens/SideMenu/SideMenu.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export class SideMenu extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Side Menu </Text>
+      </View>
+    );
+  }
+}
+
+export default SideMenu;
