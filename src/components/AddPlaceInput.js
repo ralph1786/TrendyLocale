7f7f61950c2218ee8e666d7ef6fe77961067266e
ralph1786 <ralph1786@gmail.com>
2019-05-17 11:59:34 -0400

    removed adding new location functionality

--- src/components/AddPlaceInput.js
+++ src/components/AddPlaceInput.js
@@ -1,56 +1,14 @@
-import React, { Component } from "react";
-import { View, TextInput, StyleSheet, Button } from "react-native";
+import React from "react";
+import ReusableInput from "./UI/ReusableInput";
 
-export class AddPlaceInput extends Component {
-  state = {
-    locationName: ""
-  };
-
-  changeHandler = name => {
-    this.setState({
-      locationName: name
-    });
-  };
-
-  locationSubmitHandler = () => {
-    if (this.state.locationName.trim() === "") {
-      return;
-    }
-    this.props.addLocation(this.state.locationName);
-  };
-
-  render() {
-    return (
-      <View style={styles.inputContainer}>
-        <TextInput
-          value={this.state.locationName}
-          onChangeText={this.changeHandler}
-          placeholder="enter location"
-          style={styles.textInputStyling}
-        />
-        <Button
-          title="Add"
-          style={styles.buttonStyling}
-          onPress={this.locationSubmitHandler}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "100%",
-    alignItems: "center"
-  },
-  textInputStyling: {
-    width: "70%"
-  },
-  buttonStyling: {
-    width: "30%"
-  }
-});
+const AddPlaceInput = props => {
+  return (
+    <ReusableInput
+      placeholder="Location Name"
+      value={props.locationName}
+      onChangeText={props.changeHandler}
+    />
+  );
+};
 
 export default AddPlaceInput;
