ac29c90dce95ec86dfdf5481d2589c90f2912cfc
ralph1786 <ralph1786@gmail.com>
2019-05-17 11:56:39 -0400

    added icon and styling

--- src/screens/SideMenu/SideMenu.js
+++ src/screens/SideMenu/SideMenu.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+
+import Icon from "react-native-vector-icons/Ionicons";
 
 export class SideMenu extends Component {
   render() {
     return (
-      <View>
-        <Text> Side Menu </Text>
-      </View>
+      <TouchableOpacity>
+        <View style={styles.container}>
+          <Icon
+            name="ios-log-out"
+            size={25}
+            color="#21b8ef"
+            style={styles.iconStyling}
+          />
+          <Text style={styles.textStyling}>Logout</Text>
+        </View>
+      </TouchableOpacity>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: "20%",
+    marginLeft: "10%"
+  },
+  iconStyling: {
+    marginRight: 10
+  },
+  textStyling: {
+    fontSize: 18
+  }
+});
 
 export default SideMenu;
