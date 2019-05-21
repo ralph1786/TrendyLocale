a4d3b5374c084904d9196cee4b44590def97d971
ralph1786 <ralph1786@gmail.com>
2019-05-21 10:57:18 -0400

    added feature to pick your location display in map

--- src/components/LocationPicker.js
+++ src/components/LocationPicker.js
@@ -1,15 +1,83 @@
 import React, { Component } from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet, Dimensions } from "react-native";
+import MapView from "react-native-maps";
 
-export class LocationPicker extends Component {
+class LocationPicker extends Component {
+  state = {
+    pickedLocation: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0122,
+      longitudeDelta:
+        (Dimensions.get("window").width / Dimensions.get("window").height) *
+        0.0122
+    },
+    pickedLocationMarker: false
+  };
+
+  locationPickedHandler = e => {
+    let coordinates = e.nativeEvent.coordinate;
+    //line below (this.map) is referencing the ref prop in MapView component.
+    //The code below is animate map when a marker is placed.
+    this.map.animateToRegion({
+      ...this.state.pickedLocation,
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude
+    });
+    this.setState(prevState => {
+      return {
+        pickedLocation: {
+          ...prevState.pickedLocation,
+          latitude: coordinates.latitude,
+          longitude: coordinates.longitude
+        },
+        pickedLocationMarker: true
+      };
+    });
+    this.props.locationPicked({
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude
+    });
+  };
+
+  locateUserHandler = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const coordsEvent = {
+          nativeEvent: {
+            coordinate: {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            }
+          }
+        };
+        this.locationPickedHandler(coordsEvent);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  };
+
   render() {
+    let marker = null;
+
+    if (this.state.pickedLocationMarker) {
+      marker = <MapView.Marker coordinate={this.state.pickedLocation} />;
+    }
+
     return (
       <View style={styles.container}>
-        <View style={styles.imageMapPlaceholder}>
-          <Text>Map</Text>
-        </View>
+        <MapView
+          initialRegion={this.state.pickedLocation}
+          style={styles.mapStyling}
+          onPress={this.locationPickedHandler}
+          ref={ref => (this.map = ref)}
+        >
+          {marker}
+        </MapView>
         <View style={styles.buttonStyling}>
-          <Button title="Current Location" />
+          <Button title="Current Location" onPress={this.locateUserHandler} />
         </View>
       </View>
     );
@@ -21,12 +89,9 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center"
   },
-  imageMapPlaceholder: {
-    borderColor: "blue",
-    borderWidth: 1,
-    width: "75%",
-    height: 150,
-    backgroundColor: "#eee"
+  mapStyling: {
+    width: "100%",
+    height: 250
   },
   buttonStyling: {
     margin: 6
